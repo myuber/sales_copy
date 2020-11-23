5fc4b90c4df798b3a764ad2bf49b84948dcf1f73
myuber <ogari.gari.za@gmail.com>
2020-11-23 15:43:20 +0900

    [Add] 編集と削除機能を実装しました

--- copystock/copystockApp/urls.py
+++ copystock/copystockApp/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("new/", views.new, name="new"),
+    path("article/<int:pk>/", views.view_article, name="view_article"),
+    path("article/<int:pk>/edit/", views.edit, name="edit"),
+    path("article/<int:pk>/delete/", views.delete, name="delete"),
 ]
--- copystock/copystockApp/views.py
+++ copystock/copystockApp/views.py
@@ -1,13 +1,57 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import Http404
 from . import models
 
+# トップページ（部屋の一覧）
 def index(request):
     template_name = "copystockApp/index.html"
     context = {"articles": models.Article.objects.all()}
     return render(request, template_name, context)
 
+
+# 部屋の作成
 def new(request):
     template_name = "copystockApp/new.html"
     if request.method == "POST":
-        models.Article.objects.create(title=request.POST["title"])
+        article = models.Article.objects.create(title=request.POST["title"])
+        return redirect(view_article, article.pk)
+    
     return render(request, template_name)
+
+
+# 部屋の詳細ページ
+def view_article(request, pk):
+    template_name = "copystockApp/view_article.html"
+    try:
+        article = models.Article.objects.get(pk=pk)
+    except models.Article.DoesNotExist:
+        raise Http404
+    
+    context = {"article": article}
+    return render(request, template_name, context)
+
+# 部屋の編集ページ
+def edit(request, pk):
+    template_name = "copystockApp/edit.html"
+    try:
+        article = models.Article.objects.get(pk=pk)
+    except models.Article.DoesNotExist:
+        raise Http404
+
+    if request.method == "POST":
+        article.title = request.POST["title"]
+        article.save()
+        return redirect(view_article, pk)
+    
+    context = {"article": article}
+    return render(request, template_name, context)
+
+# 部屋の削除
+def delete(request, pk):
+    try:
+        article = models.Article.objects.get(pk=pk)
+    except models.Article.DoesNotExist:
+        raise Http404
+
+    article.delete()
+    return redirect(index)
